f5cb9c2eef33d4f2f8b4e169199f218189ff4d07
baharlawter <baharlawter@gmail.com>
2025-03-07 09:31:08 -0600

    change of variables

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -23,10 +23,22 @@ Things to reflect on:
   - How do clear variable names benefit team collaboration?
   
 */
+jj
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
 
-console.log(d);
+// let a = "Alice";
+// let b = 5;
+// let c = 20;
+// let d = a + " bought " + b + " items for $" + c + ".";
+
+// console.log(d);
+
+let name="Alice";
+let numberOfItems=5;
+let totalAmount=20;
+let transactionDetails=name+"bouthg"+numberOfItems+"items for $"+""+totalAmount+".";
+console.log(transactionDetails);
+
+// To use meanigful vaiable names a lot for the other programmers to understand the code easily.
+// Avoid usng single letters as a varable name. The pittfalls to avoid when nameing vaiables are using single letter.
+// Clear vaiable name benefit team collaborating by maing the code easy to understand. 
